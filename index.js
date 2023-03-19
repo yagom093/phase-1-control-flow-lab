c100f50b179dfd39032ec96a24e0d091cb85041d
yago <yagom10@hotmail.com>
2023-03-19 02:10:33 -0700

    complete

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+if (distance <= 400){
+    return 'This one is on me!';
+  }
+
+    else if (distance > 400 && distance <= 2000){
+      return 'That will be twenty bucks.';
+    }
+
+    else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+}
+    else if (distance >= 2500){
+    return 'No can do.';
+    }
+    }
+
+  
+function ternaryCheckCity(city){
+  // Write your code here!
+if (city === "NYC") {
+  return'Ok, sounds good.' 
+}
+  else { return 'No go.';
+
+  }
+
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+switch (tip)  {
+  case 'generous' :
+  return 'Thank you so much.'
+  case 'not as generous' :
+    return 'Thank you.'
+    default :
+    return 'Bye.'
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  
 }
